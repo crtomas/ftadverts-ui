1a41484b42451e4b5b67f7b4720f97dfd50bc98b
Tomas <tcarneado@gmail.com>
2019-01-06 19:06:47 +0100

    ssi routing fix

--- server.js
+++ server.js
@@ -8,9 +8,5 @@ const HOST = '0.0.0.0';
 const app = express();
 app.use(express.static('public'));
 
-app.use('/ftadvert-wcjs', (req, res) => {
-	res.sendFile('./public/ft-advert.js', { root : __dirname});
-});
-
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
