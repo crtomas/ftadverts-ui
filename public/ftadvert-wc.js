d06cadcf615e5860f66dc22ecc2b940bbba63aa0
Tomas <tcarneado@gmail.com>
2019-01-19 19:19:10 +0100

    get api version

--- public/ftadvert-wc.js
+++ public/ftadvert-wc.js
@@ -1,6 +1,15 @@
 class FTAdvertDashboard extends HTMLElement {
  constructor() {
     super();
+	this._dataUrl = "https://ftadvert_api/";
+	this._api;
+	
+	this.fetchData().then((json) => {
+		if(JSON.stringify(json) !== JSON.stringify({})){
+			this._api = json;
+			this.render();
+		}
+	});
 
     const shadowRoot = this.attachShadow({mode: 'closed'});
     shadowRoot.innerHTML = `
@@ -10,7 +19,22 @@ class FTAdvertDashboard extends HTMLElement {
                 }
             </style>
             <p>FTAdvert v1015!</p>
+			<p id="api_version">Loading ...</p>
     `;
+  }
+  fetchData(){
+  	return fetch(this._dataUrl)
+  		.then(function(response) {
+  			return response.json();
+  		})
+  		.catch(function(error) {
+  			return {}; 
+  		});        
+  }
+  render(){
+	const $p = document.getElementById("api_version");
+	$p.innerHTML = this._api.api;
+
   }
 }
 window.customElements.define("ftadvert-dashboard", FTAdvertDashboard);
